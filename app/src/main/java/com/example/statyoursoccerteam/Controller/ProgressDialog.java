4497602870f74429d183b680a7f03e492aa73d1f
nickzznl <nick.sluiters@gmail.com>
2020-01-14 19:24:41 +0100

    Cleaned MainActivity

---
+++ app/src/main/java/com/example/statyoursoccerteam/Controller/ProgressDialog.java
@@ -0,0 +1,39 @@
+package com.example.statyoursoccerteam.Controller;
+
+import android.content.Context;
+
+public class ProgressDialog {
+    private static android.app.ProgressDialog mProgressDialog;
+
+    public static void removeSimpleProgressDialog() {
+        try {
+            if (mProgressDialog != null) {
+                if (mProgressDialog.isShowing()) {
+                    mProgressDialog.dismiss();
+                    mProgressDialog = null;
+                }
+            }
+        } catch (Exception ie) {
+            ie.printStackTrace();
+        }
+    }
+
+    public static void showSimpleProgressDialog(Context context, String title,
+                                                String msg, boolean isCancelable) {
+        try {
+            if (mProgressDialog == null) {
+                mProgressDialog = android.app.ProgressDialog.show(context, title, msg);
+                mProgressDialog.setCancelable(isCancelable);
+            }
+
+            if (!mProgressDialog.isShowing()) {
+                mProgressDialog.show();
+            }
+
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+        }
+
+
+    }
+}
